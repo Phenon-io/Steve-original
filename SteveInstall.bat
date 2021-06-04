39d5ed88c2c1f79a5c409127dc5e08dac57280ef
Phenon-io <82192013+Phenon-io@users.noreply.github.com>
2021-06-04 16:34:30 -0700

    Add files via upload
    Bat files for install and debugging

---
+++ SteveInstall.bat
@@ -0,0 +1,6 @@
+mkdir PyEnviron\Steveenviron
+py -m venv c:\PyEnviron\Steveenviron
+PyEnviron\Steveenviron\Scripts\activate
+py -m pip install requests
+py -m pip install bs4
+py -m pip install googlesearch-python
